d9004794b4ad07c320f852e8bac3633c0d9bdacb
04rookie <atharva.ek@gmail.com>
2023-08-27 23:48:38 +0530

    temporary changes

--- src/app/CardMain.js
+++ src/app/CardMain.js
@@ -4,8 +4,8 @@ import moment from "moment";
 export default function CardMain(props) {
   // console.log(props.measure);
   return (
-    <Link href="#" underline="hover">
-      <Card asChild size="2">
+    <Link href="" underline="hover">
+      <Card size="2" asChild>
         <Flex direction="column" justify="between" height="100%">
           <Text as="div" size="2" weight="bold">
             {props?.measure?.short_name}
@@ -13,7 +13,7 @@ export default function CardMain(props) {
           <Text as="div" color="gray" size="2">
             {props?.measure?.full_name}
           </Text>
-          <Box grow="1">
+          <Box>
             <Separator my="3" orientation="horizontal" size="4" />
             <Text mt="auto" as="div" color="gray" size="2">
               {moment(props?.measure?.updated_on).format(
