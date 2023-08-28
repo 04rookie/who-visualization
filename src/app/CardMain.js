58d548951c384f61264d9edfe0e336f5fd15465d
04rookie <atharva.ek@gmail.com>
2023-08-28 15:39:18 +0530

    Making cards clickable, redirect to new page

--- src/app/CardMain.js
+++ src/app/CardMain.js
@@ -1,24 +1,28 @@
-import { Box, Card, Flex, Link, Separator, Text } from "@radix-ui/themes";
+import { MeasureProperty } from "@/Component/MeasureProperty";
+import { Box, Card, Flex, Separator, Text } from "@radix-ui/themes";
 import moment from "moment";
+import Link from "next/link";
 
-export default function CardMain(props) {
+export default async function CardMain(props) {
   // console.log(props.measure);
   return (
-    <Card asChild>
-      <Flex direction="column" justify="between">
-        <Text mt="auto" as="div" color="gray" size="2">
-          {moment(props?.measure?.updated_on).format(
-            "ddd, MMMM Do YYYY, h:mm a"
-          )}
-        </Text>
-        <Separator mt="1" mb="4" orientation="horizontal" size="4" />
-        <Text as="div" size="2" weight="bold">
-          {props?.measure?.short_name}
-        </Text>
-        <Text as="div" color="gray" size="2">
-          {props?.measure?.full_name}
-        </Text>
-      </Flex>
-    </Card>
+    <Link href={`/${props?.measure?.code}`}>
+      <Card asChild>
+        <Flex direction="column" justify="between">
+          <Text mt="auto" as="div" color="gray" size="2">
+            {moment(props?.measure?.updated_on).format(
+              "ddd, MMM Do YYYY, h:mm a"
+            )}
+          </Text>
+          <Separator mt="1" mb="4" orientation="horizontal" size="4" />
+          <Text as="div" size="2" weight="bold">
+            {props?.measure?.short_name}
+          </Text>
+          <Text as="div" color="gray" size="2">
+            {props?.measure?.full_name}
+          </Text>
+        </Flex>
+      </Card>
+    </Link>
   );
 }
