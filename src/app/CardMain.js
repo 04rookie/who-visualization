45ec613ea271a229cfd82f797a0889e8a2ea56e3
04rookie <atharva.ek@gmail.com>
2023-08-28 12:19:32 +0530

    Card restructure

--- src/app/CardMain.js
+++ src/app/CardMain.js
@@ -4,25 +4,21 @@ import moment from "moment";
 export default function CardMain(props) {
   // console.log(props.measure);
   return (
-    <Link href="" underline="hover">
-      <Card size="2" asChild>
-        <Flex direction="column" justify="between" height="100%">
-          <Text as="div" size="2" weight="bold">
-            {props?.measure?.short_name}
-          </Text>
-          <Text as="div" color="gray" size="2">
-            {props?.measure?.full_name}
-          </Text>
-          <Box>
-            <Separator my="3" orientation="horizontal" size="4" />
-            <Text mt="auto" as="div" color="gray" size="2">
-              {moment(props?.measure?.updated_on).format(
-                "dddd, MMMM Do YYYY, h:mm a"
-              )}
-            </Text>
-          </Box>
-        </Flex>
-      </Card>
-    </Link>
+    <Card asChild>
+      <Flex direction="column" justify="between">
+        <Text mt="auto" as="div" color="gray" size="2">
+          {moment(props?.measure?.updated_on).format(
+            "ddd, MMMM Do YYYY, h:mm a"
+          )}
+        </Text>
+        <Separator mt="1" mb="4" orientation="horizontal" size="4" />
+        <Text as="div" size="2" weight="bold">
+          {props?.measure?.short_name}
+        </Text>
+        <Text as="div" color="gray" size="2">
+          {props?.measure?.full_name}
+        </Text>
+      </Flex>
+    </Card>
   );
 }
