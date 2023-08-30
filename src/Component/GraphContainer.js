76ac0b48032d99cc2217350e94a7d9164a71033c
04rookie <atharva.ek@gmail.com>
2023-08-30 14:38:25 +0530

    dynamic container

--- src/Component/GraphContainer.js
+++ src/Component/GraphContainer.js
@@ -8,21 +8,33 @@ const LineChartComponent = dynamic(() => import("./LineChartComponent"), {
   ssr: false,
 });
 
-export default function GraphContainer({ measure, countryMapState }) {
-  let attributeMap = {};
-  const dimension = measure?.dimensions?.[0]?.values;
+export default function GraphContainer({
+  measure,
+  countryMapState,
+  graphIndex,
+}) {
+  // let attributeMap = {};
+  // console.log(graphIndex)
+  const dimension = measure?.dimensions;
+  // const dimensionSecondary = measure?.dimensions?.[1]?.values;
   const attributes = measure?.attributes?.[0]?.values;
   let countryMap = {};
-  dimension.forEach((dim, index) => {
+  dimension?.[0]?.values.forEach((dim, index) => {
     if (countryMapState?.[dim?.who_code]) {
       countryMap[dim?.who_code] = -1;
     }
   });
-  attributes.forEach((attribute, index) => {
-    attributeMap[attribute?.code] = {
-      attributeIndex: index,
-    };
+  dimension?.[1]?.values.forEach((dim, index) => {
+    if (countryMapState?.[dim?.code]) {
+      countryMap[dim?.code] = -1;
+    }
   });
+  // console.log(countryMap)
+  // attributes.forEach((attribute, index) => {
+  //   attributeMap[attribute?.code] = {
+  //     attributeIndex: index,
+  //   };
+  // });
   let graphs = attributes?.map((element) => []);
   const YEARRANGE =
     measure?.dimensions?.[3]?.upper_bound -
@@ -42,14 +54,29 @@ export default function GraphContainer({ measure, countryMapState }) {
 
   graphs.forEach((graph, graphIndex) => {
     measure?.data?.forEach((datum) => {
+      // console.log("---------------------------");
+      // console.log(datum?.attributes?.MEASURE_TYPE);
+      // console.log(attributes?.[graphIndex]?.code);
+      // console.log("---------------------------");
       if (
         datum?.dimensions?.COUNTRY?.length !== 0 &&
         datum?.dimensions?.COUNTRY !== undefined &&
-        countryMap?.[datum?.dimensions?.COUNTRY] !== undefined
+        countryMap?.[datum?.dimensions?.COUNTRY] !== undefined &&
+        datum?.attributes?.MEASURE_TYPE === attributes?.[graphIndex]?.code
       ) {
         const diff =
           datum?.dimensions?.YEAR - measure?.dimensions?.[3]?.lower_bound;
-        graphs[graphIndex][diff].yData[datum?.dimensions?.COUNTRY] =
+        graph[diff].yData[datum?.dimensions?.COUNTRY] = datum?.value?.numeric;
+      } else if (
+        datum?.dimensions?.COUNTRY_GRP?.length !== 0 &&
+        datum?.dimensions?.COUNTRY_GRP !== undefined &&
+        countryMap?.[datum?.dimensions?.COUNTRY_GRP] !== undefined &&
+        datum?.attributes?.MEASURE_TYPE === attributes?.[graphIndex]?.code
+      ) {
+        // console.log(datum?.attributes?.MEASURE_TYPE);
+        const diff =
+          datum?.dimensions?.YEAR - measure?.dimensions?.[3]?.lower_bound;
+        graph[diff].yData[datum?.dimensions?.COUNTRY_GRP] =
           datum?.value?.numeric;
       }
     });
@@ -79,13 +106,14 @@ export default function GraphContainer({ measure, countryMapState }) {
   for (let count = 0; count < YEARRANGE; count++) {
     tempLabel.push(measure?.dimensions?.[3]?.lower_bound + count);
   }
-
+  // console.log(graphs)
   return (
     <div className="h-full w-5/6 flex">
       <div className="h-full w-full m-auto">
         <LineChartComponent
+          graphIndex={graphIndex}
           tempLabel={tempLabel}
-          dimension={dimension}
+          dimension={dimension?.[graphIndex]?.values}
           random_rgba={random_rgba}
           graphs={graphs}
         />
