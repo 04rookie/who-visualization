03ee5f9ed4ad84c557c0412f6d3f3cea62ca7dfb
04rookie <atharva.ek@gmail.com>
2023-08-30 15:13:26 +0530

    documentation for MeasureProperty comp

--- src/Component/MeasureProperty.js
+++ src/Component/MeasureProperty.js
@@ -4,23 +4,29 @@ import Sidebar from "./Sidebar";
 import { useEffect, useState } from "react";
 
 export default function MeasureProperty({ measure }) {
+  //This will store all the countries and country groups, only true values will be rendered in the graph.
   const [countryMapState, setCountryMapState] = useState(null);
+
+  //This stores index value of which graph to display. (Basically which attribute)
   const [graphIndex, setGraphIndex] = useState(0);
 
+  //Side effect to initalise all the values with true value in countryMapState, only first N (4) values will stay true by default.
   useEffect(() => {
     const dimension = measure?.dimensions;
     let countryMapTemp = {};
+    //first iterate through all countries
     dimension?.[0]?.values.forEach((dim, index) => {
-      // countryMapTemp[dim?.who_code] = -1;
+      //change this to set how many countries should be rendered by defualt when graph first mounts.
       if (index < 4) {
         countryMapTemp[dim?.who_code] = true;
       } else {
         countryMapTemp[dim?.who_code] = false;
       }
     });
-    dimension?.[1]?.values.forEach((dim, index)=>{
-      countryMapTemp[dim?.code] = false
-    })
+    //second iterate through all country groups
+    dimension?.[1]?.values.forEach((dim, index) => {
+      countryMapTemp[dim?.code] = false;
+    });
     setCountryMapState(countryMapTemp);
   }, [measure]);
   return (
